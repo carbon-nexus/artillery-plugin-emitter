a63c20981a2623c1730cf0272db9c8ec954402fd
Jon McLean <jmclean@constellation.coop>
2020-04-23 14:32:17 -0400

    Added AWS SNS Support

---
+++ src/errors/broker-emitter.ts
@@ -0,0 +1,1 @@
+export class BrokerEmitterError extends Error {}
--- src/interfaces/broker-emitter.ts
+++ src/interfaces/broker-emitter.ts
@@ -1,7 +1,19 @@
 export interface BrokerEmitterConfig {
-    broker: "aws"
+    vendor: "aws",
+    broker: "sns",
+    sns?: SnsBrokerEmitter,
+    loggingLevel: number
+}
+
+export interface BrokerEventAttributes {
+    type: string,
+    source: string
 }
 
 export interface ArtilleryEventEmitter {
-    on: (event: string) => void;
+    on: (event: string, cb: (data: any)=>void ) => void;
+}
+
+export interface SnsBrokerEmitter {
+    arn: string
 }
